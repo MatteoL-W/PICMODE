2533680e65a946bb8899e618a68dd8692cfc9b82
Matteo Leclercq <matteo.ftt@gmail.com>
2022-03-16 16:48:31 +0100

    Reorganizing project

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 require_once('controllers/RouteError.php');
 require_once('controllers/Index.php');
 
-/** Load view function */
+/** Load config files */
 require_once('config/constants.php');
-require_once('config/utilities.php');
 require_once('config/credentials.php');
 
-/** Load routes and routes Handler */
+/** Load routes and routes Handler -> start the page */
+require_once('app/viewsHandler.php');
 require_once('app/routesHandler.php');
