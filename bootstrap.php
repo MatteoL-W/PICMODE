6e05912d31b71da261653dd2fede2fc828de32a8
Matteo Leclercq <matteo.ftt@gmail.com>
2022-03-17 18:04:15 +0100

    Moving require in bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,11 @@
 <?php
 
 require_once dirname(__FILE__) . '/vendor/autoload.php';
+
+/** Load config files */
+require_once('config/constants.php');
+require_once('config/credentials.php');
+
+/** Load routes and routes Handler -> start the page */
+require_once('app/viewsHandler.php');
+require_once('app/routesHandler.php');
