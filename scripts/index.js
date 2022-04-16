66f5bb57ad012a1b71fbd13b582442cec194fd08
Matteo Leclercq <matteo.ftt@gmail.com>
2022-04-16 21:49:30 +0200

    adding to base64 *

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,25 @@
-import {useFetch} from "./modules/fetchTools";
+import {useFetch, toBase64} from "./modules/fetchTools";
 
 window.addEventListener('DOMContentLoaded', () => {
     useFetch('http://localhost/S2_PHP/api/example', 'GET').then(results => {
         console.log(results)
+    })
+
+    const form = document.querySelector('.form')
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        let working = true;
+        let image = document.querySelector('.image').files[0]
+        const result = await toBase64(image).catch(e => Error(e));
+        if (result instanceof Error) {
+            working = false
+            console.log('Error: ', result.message);
+        }
+
+        if (working) {
+            document.querySelector('.dynamic').src = result
+            document.querySelector('iframe').contentDocument.open().write(result);
+        }
     })
 
     /*useFetch('http://localhost/S2_PHP/api/example/7', 'DELETE').then(results => {
