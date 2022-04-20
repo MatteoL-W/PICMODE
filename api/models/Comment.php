e91b9e162c602855667d40276dfbe107ea04fc64
Elise Massa <elise.massa.perso@gmail.com>
2022-04-20 14:55:10 +0200

    comment maj

--- api/models/Comment.php
+++ api/models/Comment.php
@@ -15,61 +15,12 @@ class Comment
 
     public function create(string $text): bool
     {
-        $this->db->query("INSERT INTO comment (`text`) VALUES (:text)");
-        $this->db->bind(":text", $text);
+        $values = [$text];
+        $keys = ['text'];
 
-        if ($this->db->execute()) {
-            return true;
-        }
+        return $this->createFromArray($values, $keys);
 
-        return false;
     }
 
-    public function selectAll(): array
-    {
-        $this->db->query("SELECT * FROM comment");
-        return $this->db->fetchAll();
-    }
 
-    public function select(int $id)
-    {
-        $this->db->query("SELECT * FROM comment WHERE id = :id");
-        $this->db->bind(':id', $id);
-        return $this->db->fetch();
-    }
-
-    public function delete(int $id): bool
-    {
-        $this->db->query("DELETE FROM comment WHERE id = :id");
-        $this->db->bind(':id', $id);
-
-        if ($this->db->execute()) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public function update(int $id, stdClass $fields): bool
-    {
-        $setValue = '';
-        $i = 0;
-
-        foreach ($fields as $name => $value) {
-            $setValue .= "{$name} = '{$value}'";
-            if ($i < count((array)$fields) - 1) {
-                $setValue .= ', ';
-            }
-            $i++;
-        }
-
-        $this->db->query("UPDATE comment SET $setValue WHERE id = :id");
-        $this->db->bind(':id', $id);
-
-        if ($this->db->execute()) {
-            return true;
-        }
-
-        return false;
-    }
 }
