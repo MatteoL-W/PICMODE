35a62eb3a4e36155e5b2118cee0d0c8dfd45806c
grignolanina <grignola.nina@orange.fr>
2022-05-03 11:12:24 +0200

    post jointure-relation modified

--- api/models/Post.php
+++ api/models/Post.php
@@ -25,22 +25,22 @@ class Post extends BaseModel
     //Méthode pour compter tous les posts d'un User
     public function countPostsFromUserId(int $idUser)
     {
-        $this->db->query("SELECT COUNT(*) FROM post WHERE id = :id");
+        $this->db->query("SELECT COUNT(*) FROM post WHERE idUser = :id");
         $this->db->bind(':id', $idUser);
         return $this->db->fetchAll();
     }
 
     //Méthode pour selectionner tous les posts publiés
     public function selectAllPostsWithUsers(int $idUser){
-        $this->db->query("SELECT post.*, user.pseudo FROM post INNER JOIN user ON post.idUser = user.id ORDER BY date DESC");
-        $this->db->bind();
+        $this->db->query("SELECT post.*, user.pseudo FROM post INNER JOIN user ON post.idUser = user.id WHERE idUser=':id' ORDER BY date DESC");
+        $this->db->bind(':id', $idUser);
         return $this->db->fetchAll();
     }
 
     //Méthode pour selectionner tous les posts publiés par les Users suivies
     public function selectAllFollowedPostsFromLoggedUser (int $idUser){
-        $this->db->query("SELECT post.*, user.pseudo FROM post INNER JOIN user ON post.idUser=user.id INNER JOIN following ON user.id=following.idFollowing ORDER BY date DESC");
-        $this->db->bind();
+        $this->db->query("SELECT post.*, user.pseudo FROM post INNER JOIN user ON post.idUser=user.id INNER JOIN following ON user.id=following.idFollowing WHERE idUser=':id' ORDER BY date DESC");
+        $this->db->bind(':id', $idUser);
         return $this->db->fetchAll();
     }
 }
