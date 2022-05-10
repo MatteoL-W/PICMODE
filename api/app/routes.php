1f679bf67b208e2b765978b6183c0a031644769b
Emily-Rose <er.strich@hotmail.fr>
2022-05-10 12:14:44 +0200

    finitions de contains et verif

--- api/app/routes.php
+++ api/app/routes.php
@@ -54,4 +54,8 @@ array_push($router, ['/following/countFollowing/{id}/', ['controller' => 'Follow
 array_push($router, ['/following/', ['controller' => 'Following', 'action' => 'create', 'method' => 'post', 'desc' => 'Create a new following<br>(send idFollower and idFollowing in query)']]);
 array_push($router, ['/following/{id}/{id2}/', ['controller' => 'Following', 'action' => 'delete', 'method' => 'delete', 'desc' => 'Delete a following<br>(id = idFollower ; id2 = idFollowing)']]);
 
-
+array_push($router, ['/contains/', ['controller' => 'Contains', 'action' => 'read', 'method' => 'get', 'desc' => 'Return all the contains']]);
+array_push($router, ['/contains/getClothing/{id}/', ['controller' => 'Contains', 'action' => 'getClothing', 'method' => 'get', 'desc' => 'Get all the clothing of a post']]);
+array_push($router, ['/contains/countClothing/{id}/', ['controller' => 'Contains', 'action' => 'countClothing', 'method' => 'get', 'desc' => 'Count the clothing of a post']]);
+array_push($router, ['/contains/', ['controller' => 'Contains', 'action' => 'create', 'method' => 'post', 'desc' => 'Create a new contains<br>(send idPost and idClothing in query)']]);
+array_push($router, ['/contains/{id}/{id2}/', ['controller' => 'Contains', 'action' => 'delete', 'method' => 'delete', 'desc' => 'Delete a contains<br>(id = idPost ; id2 = idClothing)']]);
